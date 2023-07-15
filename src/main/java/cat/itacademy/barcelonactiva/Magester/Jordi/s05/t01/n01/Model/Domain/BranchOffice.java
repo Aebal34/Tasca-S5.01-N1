2782a8af3d1f5a5e0dd691575b2a11c86d8093ad
Joren <97386268+Aebal34@users.noreply.github.com>
2023-07-15 14:05:48 +0200

    BranchOffice Entity and project structure

---
+++ src/main/java/cat/itacademy/barcelonactiva/Magester/Jordi/s05/t01/n01/Model/Domain/BranchOffice.java
@@ -0,0 +1,18 @@
+package cat.itacademy.barcelonactiva.Magester.Jordi.s05.t01.n01.Model.Domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class BranchOffice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer pk_BranchOfficeID;
+
+    String branchOfficeName;
+
+    String branchOfficeCountry;
+}
